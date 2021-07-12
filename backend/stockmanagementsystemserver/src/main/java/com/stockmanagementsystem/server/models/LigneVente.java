69e22fb4f53b0559d76b04a9a65a530fad761cc4
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-07-12 12:42:32 +0200

    Task 26: update lignevente entity (make relationship)

--- backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/models/LigneVente.java
+++ backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/models/LigneVente.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.math.BigDecimal;
 
 /**
  * @Created 12/07/2021 - 09:06
@@ -24,4 +25,10 @@ public class LigneVente extends AbstractEntity {
     @ManyToOne(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
     @JoinColumn(name = "ventes_id")
     private Ventes ventes;
+
+    @Column(name = "quantite")
+    private BigDecimal quantite;
+
+    @Column(name = "prix_unitaire")
+    private BigDecimal prixUnitaire;
 }
