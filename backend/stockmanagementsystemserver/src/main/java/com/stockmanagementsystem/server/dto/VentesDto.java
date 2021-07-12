c0fd5da2464f3d046a0da069f4648410922e9c08
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-07-12 15:57:57 +0200

    Task 45: update ventesdto  (add fromentity & toentity methodes)

--- backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/dto/VentesDto.java
+++ backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/dto/VentesDto.java
@@ -1,5 +1,7 @@
 package com.stockmanagementsystem.server.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.stockmanagementsystem.server.models.Ventes;
 import lombok.Builder;
 import lombok.Data;
 
@@ -20,5 +22,29 @@ public class VentesDto {
     private String code;
     private Date dateVente;
     private String commentaire;
+
+    @JsonIgnore
     private List<LigneVenteDto> ligneVenteDtoList;
+
+    public static VentesDto fromEntity(Ventes vente) {
+        if (vente == null) {
+            return null;
+        }
+        return VentesDto.builder()
+                .id(vente.getId())
+                .code(vente.getCode())
+                .commentaire(vente.getCommentaire())
+                .build();
+    }
+
+    public static Ventes toEntity(VentesDto ventesDto) {
+        if (ventesDto == null) {
+            return null;
+        }
+        Ventes ventes = new Ventes();
+        ventes.setId(ventesDto.getId());
+        ventes.setCode(ventes.getCode());
+        ventes.setCommentaire(ventesDto.getCommentaire());
+        return ventes;
+    }
 }
