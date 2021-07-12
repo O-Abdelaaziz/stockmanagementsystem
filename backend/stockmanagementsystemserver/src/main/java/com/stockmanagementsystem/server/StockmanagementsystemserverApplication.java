4bfd118a7c248251188f2ecbabfa4fad5a11efd4
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-07-12 08:39:26 +0200

    Task 1: first commit & upload project

---
+++ backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/StockmanagementsystemserverApplication.java
@@ -0,0 +1,13 @@
+package com.stockmanagementsystem.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockmanagementsystemserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockmanagementsystemserverApplication.class, args);
+    }
+
+}
