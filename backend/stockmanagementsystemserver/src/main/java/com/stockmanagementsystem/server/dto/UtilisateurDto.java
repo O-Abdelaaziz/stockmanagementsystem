ed87b310f7f30c2e6d98dfbc502deb2980e262eb
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-07-12 16:04:37 +0200

    Task 48: update utilisateurdto  (add fromentity & toentity methodes)

--- backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/dto/UtilisateurDto.java
+++ backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/dto/UtilisateurDto.java
@@ -1,10 +1,12 @@
 package com.stockmanagementsystem.server.dto;
 
+import com.stockmanagementsystem.server.models.Utilisateur;
 import lombok.Builder;
 import lombok.Data;
 
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @Created 12/07/2021 - 14:14
@@ -26,4 +28,47 @@ public class UtilisateurDto {
     private AdresseDto adresseDto;
     private EntrepriseDto entrepriseDto;
     private List<RolesDto> rolesDtoList;
+
+    public static UtilisateurDto fromEntity(Utilisateur utilisateur) {
+        if (utilisateur == null) {
+            return null;
+        }
+
+        return UtilisateurDto.builder()
+                .id(utilisateur.getId())
+                .nom(utilisateur.getNom())
+                .prenom(utilisateur.getPrenom())
+                .email(utilisateur.getEmail())
+                .moteDePasse(utilisateur.getMoteDePasse())
+                .dateDeNaissance(utilisateur.getDateDeNaissance())
+                .adresseDto(AdresseDto.fromEntity(utilisateur.getAdresse()))
+                .photo(utilisateur.getPhoto())
+                .entrepriseDto(EntrepriseDto.fromEntity(utilisateur.getEntreprise()))
+//                .rolesDtoList(
+//                        utilisateur.getRoles() != null ?
+//                                utilisateur.getRoles().stream()
+//                                        .map(RolesDto::fromEntity)
+//                                        .collect(Collectors.toList()) : null
+//                )
+                .build();
+    }
+
+    public static Utilisateur toEntity(UtilisateurDto dto) {
+        if (dto == null) {
+            return null;
+        }
+
+        Utilisateur utilisateur = new Utilisateur();
+        utilisateur.setId(dto.getId());
+        utilisateur.setNom(dto.getNom());
+        utilisateur.setPrenom(dto.getPrenom());
+        utilisateur.setEmail(dto.getEmail());
+        utilisateur.setMoteDePasse(dto.getMoteDePasse());
+        utilisateur.setDateDeNaissance(dto.getDateDeNaissance());
+        utilisateur.setAdresse(AdresseDto.toEntity(dto.getAdresseDto()));
+        utilisateur.setPhoto(dto.getPhoto());
+        utilisateur.setEntreprise(EntrepriseDto.toEntity(dto.getEntrepriseDto()));
+
+        return utilisateur;
+    }
 }
