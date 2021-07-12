02778ad5924a196bef47235e3e1c0d90e9691049
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-07-12 09:56:55 +0200

    Task 11: add adresse entity

---
+++ backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/models/Adresse.java
@@ -0,0 +1,11 @@
+package com.stockmanagementsystem.server.models;
+
+/**
+ * @Created 12/07/2021 - 09:55
+ * @Package com.stockmanagementsystem.server.models
+ * @Project stockmanagementsystemserver
+ * @User LegendDZ
+ * @Author Abdelaaziz Ouakala
+ **/
+public class Adresse {
+}
