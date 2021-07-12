3b40fa10f5644427363774ced0cadcee24480b68
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-07-12 15:49:41 +0200

    Task 41: update commandefournisseurdto  (add fromentity & toentity methodes)

--- backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/dto/CommandeFournisseurDto.java
+++ backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/dto/CommandeFournisseurDto.java
@@ -1,5 +1,6 @@
 package com.stockmanagementsystem.server.dto;
 
+import com.stockmanagementsystem.server.models.CommandeFournisseur;
 import com.stockmanagementsystem.server.models.Fournisseur;
 import com.stockmanagementsystem.server.models.LigneCommandeFournisseur;
 import lombok.Builder;
@@ -23,4 +24,29 @@ public class CommandeFournisseurDto {
     private Date dateCommande;
     private FournisseurDto fournisseurDto;
     private List<LigneCommandeFournisseurDto> ligneCommandeFournisseurDtoList;
+
+
+    public static CommandeFournisseurDto fromEntity(CommandeFournisseur commandeFournisseur) {
+        if (commandeFournisseur == null) {
+            return null;
+        }
+        return CommandeFournisseurDto.builder()
+                .id(commandeFournisseur.getId())
+                .code(commandeFournisseur.getCode())
+                .dateCommande(commandeFournisseur.getDateCommande())
+                .fournisseurDto(FournisseurDto.fromEntity(commandeFournisseur.getFournisseur()))
+                .build();
+    }
+
+    public static CommandeFournisseur toEntity(CommandeFournisseurDto commandeFournisseurDto) {
+        if (commandeFournisseurDto == null) {
+            return null;
+        }
+        CommandeFournisseur commandeFournisseur = new CommandeFournisseur();
+        commandeFournisseur.setId(commandeFournisseurDto.getId());
+        commandeFournisseur.setCode(commandeFournisseurDto.getCode());
+        commandeFournisseur.setDateCommande(commandeFournisseurDto.getDateCommande());
+        commandeFournisseur.setFournisseur(FournisseurDto.toEntity(commandeFournisseurDto.getFournisseurDto()));
+        return commandeFournisseur;
+    }
 }
