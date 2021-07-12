a1ad7abd2425004b2887b8a1288c4fd75dd288e8
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-07-12 15:54:58 +0200

    Task 43: update lignecommandeclientdto  (add fromentity & toentity methodes)

--- backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/dto/LigneCommandeClientDto.java
+++ backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/dto/LigneCommandeClientDto.java
@@ -1,5 +1,6 @@
 package com.stockmanagementsystem.server.dto;
 
+import com.stockmanagementsystem.server.models.LigneCommandeClient;
 import lombok.Builder;
 import lombok.Data;
 
@@ -20,4 +21,29 @@ public class LigneCommandeClientDto {
     private BigDecimal prixUnitaire;
     private ArticleDto articleDto;
     private CommandeClientDto commandeClientDto;
+
+    public static LigneCommandeClientDto fromEntity(LigneCommandeClient ligneCommandeClient) {
+        if (ligneCommandeClient == null) {
+            return null;
+        }
+        return LigneCommandeClientDto.builder()
+                .id(ligneCommandeClient.getId())
+                .articleDto(ArticleDto.fromEntity(ligneCommandeClient.getArticle()))
+                .quantite(ligneCommandeClient.getQuantite())
+                .prixUnitaire(ligneCommandeClient.getPrixUnitaire())
+                .build();
+    }
+
+    public static LigneCommandeClient toEntity(LigneCommandeClientDto ligneCommandeClientDto) {
+        if (ligneCommandeClientDto == null) {
+            return null;
+        }
+
+        LigneCommandeClient ligneCommandeClient = new LigneCommandeClient();
+        ligneCommandeClient.setId(ligneCommandeClientDto.getId());
+        ligneCommandeClient.setArticle(ArticleDto.toEntity(ligneCommandeClientDto.getArticleDto()));
+        ligneCommandeClient.setPrixUnitaire(ligneCommandeClientDto.getPrixUnitaire());
+        ligneCommandeClient.setQuantite(ligneCommandeClientDto.getQuantite());
+        return ligneCommandeClient;
+    }
 }
