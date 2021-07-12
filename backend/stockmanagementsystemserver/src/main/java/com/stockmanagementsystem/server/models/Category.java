80ee510b8aa79788f888fe3bfa2caa92b5e5ee2f
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-07-12 09:33:57 +0200

    Task 8: update category entity

--- backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/models/Category.java
+++ backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/models/Category.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -21,4 +22,10 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "categories",schema = "public")
 public class Category extends AbstractEntity {
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "designation")
+    private String designation;
 }
