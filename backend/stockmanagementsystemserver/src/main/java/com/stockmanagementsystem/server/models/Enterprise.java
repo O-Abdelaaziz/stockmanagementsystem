6111dfe7d101f15a2579a311b50867b3f794923a
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-07-12 12:53:10 +0200

    Task 28: update enterpries entity (add more fields)

--- backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/models/Enterprise.java
+++ backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/models/Enterprise.java
@@ -5,8 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
 
 /**
  * @Created 12/07/2021 - 09:08
@@ -22,5 +22,27 @@ import javax.persistence.Table;
 @Table(name = "enterprises",schema = "public")
 public class Enterprise extends AbstractEntity {
 
+    @Column(name = "nom")
     private String nom;
+
+    @Column(name = "description")
+    private String description;
+
+    @Embedded
+    private Adresse adresse;
+
+    @Column(name = "code_fiscal")
+    private String codeFiscal;
+
+    @Column(name = photo)
+    private String photo;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "site_web")
+    private String siteWeb;
+
+    @OneToMany(mappedBy = "enterprise",fetch = FetchType.LAZY,orphanRemoval = true)
+    private List<Utilisateur> utilisateurList;
 }
