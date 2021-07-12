37ca0e3ccbda83d442762166ce9805fe78dff218
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-07-12 13:25:49 +0200

    Task 31: update mouvementdestock entity (add more fields)

---
+++ backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/enums/MvtStockEnum.java
@@ -0,0 +1,12 @@
+package com.stockmanagementsystem.server.enums;
+
+/**
+ * @Created 12/07/2021 - 13:22
+ * @Package com.stockmanagementsystem.server.enums
+ * @Project stockmanagementsystemserver
+ * @User LegendDZ
+ * @Author Abdelaaziz Ouakala
+ **/
+public enum MvtStockEnum {
+    ENTEREE,SORTIE
+}
--- backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/models/MouvementDeStock.java
+++ backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/models/MouvementDeStock.java
@@ -1,11 +1,14 @@
 package com.stockmanagementsystem.server.models;
 
+import com.stockmanagementsystem.server.enums.MvtStockEnum;
 import com.stockmanagementsystem.server.models.common.AbstractEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
 
 /**
  * @Created 12/07/2021 - 09:08
@@ -21,8 +24,16 @@ import javax.persistence.*;
 @Table(name = "mouvements_de_stock",schema = "public")
 public class MouvementDeStock extends AbstractEntity {
 
+    @Column(name = "date_Mvt")
+    private Date dateMvt;
+
+    @Column(name = "quantite")
+    private BigDecimal quantite;
+
+    @Column(name = "type_mvt_stock")
+    private MvtStockEnum typeMvtStock;
+
     @ManyToOne(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
     @JoinColumn(name = "article_id")
     private Article article;
-
 }
