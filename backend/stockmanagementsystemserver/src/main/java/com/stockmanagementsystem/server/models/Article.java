e8c50c2c5f9917d03afe8fe87b99d019c2d95a49
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-07-12 09:20:13 +0200

    Task 6: add annotations for all entities

--- backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/models/Article.java
+++ backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/models/Article.java
@@ -1,6 +1,12 @@
 package com.stockmanagementsystem.server.models;
 
 import com.stockmanagementsystem.server.models.common.AbstractEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 /**
  * @Created 12/07/2021 - 09:03
@@ -9,5 +15,10 @@ import com.stockmanagementsystem.server.models.common.AbstractEntity;
  * @User LegendDZ
  * @Author Abdelaaziz Ouakala
  **/
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "articles",schema = "public")
 public class Article extends AbstractEntity {
 }
--- backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/models/LigneCommandeFournisseur.java
+++ backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/models/LigneCommandeFournisseur.java
@@ -1,6 +1,12 @@
 package com.stockmanagementsystem.server.models;
 
 import com.stockmanagementsystem.server.models.common.AbstractEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 /**
  * @Created 12/07/2021 - 09:05
@@ -9,5 +15,10 @@ import com.stockmanagementsystem.server.models.common.AbstractEntity;
  * @User LegendDZ
  * @Author Abdelaaziz Ouakala
  **/
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Lignes_commandes_fournisseurs",schema = "public")
 public class LigneCommandeFournisseur extends AbstractEntity {
 }
