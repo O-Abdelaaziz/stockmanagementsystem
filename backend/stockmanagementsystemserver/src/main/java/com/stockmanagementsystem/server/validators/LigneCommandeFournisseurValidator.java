9114bd07a508ae42231a211c30e48633322b9a59
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-07-14 12:34:33 +0200

    Task 61: add ligne commande fournisseur validator

---
+++ backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/validators/LigneCommandeFournisseurValidator.java
@@ -0,0 +1,38 @@
+package com.stockmanagementsystem.server.validators;
+
+import com.stockmanagementsystem.server.dto.LigneCommandeFournisseurDto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Created 14/07/2021 - 12:32
+ * @Package com.stockmanagementsystem.server.validators
+ * @Project stockmanagementsystemserver
+ * @User LegendDZ
+ * @Author Abdelaaziz Ouakala
+ **/
+public class LigneCommandeFournisseurValidator {
+
+    public static List<String> validate(LigneCommandeFournisseurDto ligneCommandeFournisseurDto) {
+        List<String> errors = new ArrayList<>();
+        if(ligneCommandeFournisseurDto == null){
+            errors.add("Veuillez renseigner la quantite");
+            errors.add("Veuillez renseigner le prix unitaire");
+            errors.add("Veuillez renseigner l'article");
+            errors.add("Veuillez renseigner le commande client");
+            return errors;
+        }
+
+        if (ligneCommandeFournisseurDto.getQuantite()==null) {
+            errors.add("Veuillez renseigner la quantite");
+        }
+        if (ligneCommandeFournisseurDto.getPrixUnitaire()==null) {
+            errors.add("Veuillez renseigner le prix unitaire");
+        }
+        errors.addAll(ArticleValidator.validate(ligneCommandeFournisseurDto.getArticleDto()));
+        errors.addAll(CommandeFournisseurValidator.validate(ligneCommandeFournisseurDto.getCommandeFournisseurDto()));
+
+        return errors;
+    }
+}
