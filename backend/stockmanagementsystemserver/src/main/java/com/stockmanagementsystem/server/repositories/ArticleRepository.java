23bd804a2a728e462321c1e2950ff71aa52dbda7
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-07-15 09:01:58 +0200

    Task 84: update article repository (fetch data exemples)

--- backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/repositories/ArticleRepository.java
+++ backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/repositories/ArticleRepository.java
@@ -2,7 +2,11 @@ package com.stockmanagementsystem.server.repositories;
 
 import com.stockmanagementsystem.server.models.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * @Created 14/07/2021 - 13:55
@@ -13,4 +17,16 @@ import org.springframework.stereotype.Repository;
  **/
 @Repository
 public interface ArticleRepository extends JpaRepository<Article,Long> {
+    //JPQL Query
+    @Query("select a from Article a where a.code=:code and a.designation=:designation")
+    List<Article> findByCustomQuery(@Param("code") String code, @Param("designation") String designation);
+
+    //Native Query
+    @Query("select * from article where code=:code")
+    List<Article> findByNativeQuery(@Param("code") String code);
+
+    //JPA Query Builder
+    List<Article> findByCodeIgnoreCaseAndDesignationIgnoreCase(String code,String designation);
+
+
 }
