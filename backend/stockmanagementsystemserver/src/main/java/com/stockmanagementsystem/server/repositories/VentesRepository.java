bb942c5025f153b17346e06bb75d34cc35ca0b2a
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-07-15 08:34:48 +0200

    Task 83: add ventes repository

---
+++ backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/repositories/VentesRepository.java
@@ -0,0 +1,16 @@
+package com.stockmanagementsystem.server.repositories;
+
+import com.stockmanagementsystem.server.models.Ventes;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @Created 15/07/2021 - 08:34
+ * @Package com.stockmanagementsystem.server.repositories
+ * @Project stockmanagementsystemserver
+ * @User LegendDZ
+ * @Author Abdelaaziz Ouakala
+ **/
+@Repository
+public interface VentesRepository extends JpaRepository<Ventes,Long> {
+}
