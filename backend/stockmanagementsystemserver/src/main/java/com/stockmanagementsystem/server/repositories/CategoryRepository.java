fc61621346c3aa9d78a2beed1b9bebf6f09dacfc
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-07-15 08:22:09 +0200

    Task 71: add category repository

---
+++ backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/repositories/CategoryRepository.java
@@ -0,0 +1,16 @@
+package com.stockmanagementsystem.server.repositories;
+
+import com.stockmanagementsystem.server.models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @Created 15/07/2021 - 08:20
+ * @Package com.stockmanagementsystem.server.repositories
+ * @Project stockmanagementsystemserver
+ * @User LegendDZ
+ * @Author Abdelaaziz Ouakala
+ **/
+@Repository
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+}
