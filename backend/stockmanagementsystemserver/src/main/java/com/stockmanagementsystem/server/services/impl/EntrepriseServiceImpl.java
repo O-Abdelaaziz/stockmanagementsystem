641d9e7ba2b88502573cd0df41c436a7dfb15fd9
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-07-16 11:32:40 +0200

    Task 94: complete entreprise service

--- backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/services/impl/EntrepriseServiceImpl.java
+++ backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/services/impl/EntrepriseServiceImpl.java
@@ -1,13 +1,20 @@
 package com.stockmanagementsystem.server.services.impl;
 
 import com.stockmanagementsystem.server.dto.EntrepriseDto;
+import com.stockmanagementsystem.server.exceptions.EntityNotFoundException;
+import com.stockmanagementsystem.server.exceptions.ErrorCodes;
+import com.stockmanagementsystem.server.exceptions.InvalidEntityException;
+import com.stockmanagementsystem.server.models.Entreprise;
 import com.stockmanagementsystem.server.repositories.EntrepriseRepository;
 import com.stockmanagementsystem.server.services.EntrepriseService;
+import com.stockmanagementsystem.server.validators.EntrepriseValidator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -23,21 +30,43 @@ public class EntrepriseServiceImpl implements EntrepriseService {
 
     @Override
     public List<EntrepriseDto> findAll() {
-        return null;
+        return entrepriseRepository.findAll()
+                .stream()
+                .map(EntrepriseDto::fromEntity)
+                .collect(Collectors.toList());
     }
 
     @Override
     public EntrepriseDto findById(Long id) {
-        return null;
+        if(id == null){
+            log.error("-_=> EntrepriseServiceImpl(42) => findById: entreprise id is null {}",id);
+            return null;
+        }
+        Optional<Entreprise> entrepriseOptional=entrepriseRepository.findById(id);
+        EntrepriseDto entrepriseDto=EntrepriseDto.fromEntity(entrepriseOptional.get());
+        return Optional.of(entrepriseDto).orElseThrow(()->new EntityNotFoundException("EntrepriseServiceImpl(50) => findById: no entreprise found with id: "+id, ErrorCodes.ENTREPRISE_NOT_FOUND));
     }
 
     @Override
     public EntrepriseDto save(EntrepriseDto entrepriseDto) {
-        return null;
+        List<String> errors = EntrepriseValidator.validate(entrepriseDto);
+        if(!errors.isEmpty()){
+            log.error("-_=> EntrepriseServiceImpl(54) => save: entreprise not valid {}",entrepriseDto);
+            throw new InvalidEntityException("Article not valid", ErrorCodes.ENTREPRISE_NOT_FOUND);
+        }
+        Entreprise newEntreprise=entrepriseRepository.save(EntrepriseDto.toEntity(entrepriseDto));
+        EntrepriseDto newEntrepriseDto=EntrepriseDto.fromEntity(newEntreprise);
+        return newEntrepriseDto;
     }
 
     @Override
     public void delete(Long id) {
-
+        if(id==null){
+            log.error("-_=> EntrepriseServiceImpl(65) => delete: entreprise id is null {}",id);
+            return;
+        }
+        EntrepriseDto entrepriseDto=findById(id);
+        Entreprise entreprise=EntrepriseDto.toEntity(entrepriseDto);
+        entrepriseRepository.delete(entreprise);
     }
 }
