cb59907435179d3c5d76382f48160770cc172152
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-07-22 11:13:53 +0200

    Task 109: add flickr configuration class

---
+++ backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/configs/FlickrConfiguration.java
@@ -0,0 +1,63 @@
+package com.stockmanagementsystem.server.configs;
+
+import com.flickr4java.flickr.Flickr;
+import com.flickr4java.flickr.FlickrException;
+import com.flickr4java.flickr.REST;
+import com.flickr4java.flickr.auth.Auth;
+import com.github.scribejava.apis.FlickrApi;
+import com.github.scribejava.core.builder.ServiceBuilder;
+import com.github.scribejava.core.model.OAuth1AccessToken;
+import com.github.scribejava.core.model.OAuth1RequestToken;
+import com.github.scribejava.core.oauth.OAuth10aService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * @Created 22/07/2021 - 10:28
+ * @Package com.stockmanagementsystem.server.configs
+ * @Project stockmanagementsystemserver
+ * @User LegendDZ
+ * @Author Abdelaaziz Ouakala
+ **/
+@Configuration
+public class FlickrConfiguration {
+
+    @Value("${flickr.api.key}")
+    private String apiKey;
+    @Value("${flickr.api.secret}")
+    private String apiSecret;
+
+    @Bean
+    public Flickr getFlickr() throws IOException, ExecutionException, InterruptedException, FlickrException {
+        Flickr flickr = new Flickr(apiKey, apiSecret, new REST());
+        OAuth10aService service = new ServiceBuilder(apiKey)
+                .apiSecret(apiSecret)
+                .build(FlickrApi.instance(FlickrApi.FlickrPerm.DELETE));
+
+        final Scanner scanner = new Scanner(System.in);
+        final OAuth1RequestToken request = service.getRequestToken();
+        final String authUrl = service.getAuthorizationUrl(request);
+
+        System.out.println(authUrl);
+        System.out.println("Past it here -->");
+
+        final String authVerification = scanner.nextLine();
+        OAuth1AccessToken accessToken = service.getAccessToken(request, authVerification);
+
+        System.out.println(accessToken.getToken());
+        System.out.println(accessToken.getTokenSecret());
+
+        Auth auth = flickr.getAuthInterface().checkToken(accessToken);
+
+        System.out.println("--------------");
+        System.out.println(auth.getToken());
+        System.out.println(auth.getTokenSecret());
+
+        return flickr;
+    }
+}
