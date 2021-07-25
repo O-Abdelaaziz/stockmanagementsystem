bf6b759ad270de76e85ff75e9add4b7e0e48b0f0
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-07-25 15:17:21 +0200

    Task 123: add enable_jpa_auditing annotation (main class)

--- backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/StockmanagementsystemserverApplication.java
+++ backend/stockmanagementsystemserver/src/main/java/com/stockmanagementsystem/server/StockmanagementsystemserverApplication.java
@@ -2,8 +2,10 @@ package com.stockmanagementsystem.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class StockmanagementsystemserverApplication {
 
     public static void main(String[] args) {
